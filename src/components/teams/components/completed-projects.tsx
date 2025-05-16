dc8370089e2f9c0b6b2ba776cbb4a89b1d3524e9
saad <saad@laelenergie.fr>
2025-05-16 11:06:25 +0200

    Affihcage des chantiers par equipe

--- src/components/teams/components/completed-projects.tsx
+++ src/components/teams/components/completed-projects.tsx
@@ -39,14 +39,15 @@ export function CompletedProjects({ teamID }: CompletedProjectsProps) {
   const [projectToRemove, setProjectToRemove] = useState<Project | null>(null);
   const [refreshKey, setRefreshKey] = useState(0);
 
+
+  const { data: teams } = useFirebase<Team>('teams');
+  const teamName = teams?.find(t => t.id === teamID)?.name;
+
   const { data: projects, loading } = useFirebase<Project>('projects', {
-    conditions: [{ field: 'teamId', operator: '==', value: teamID }],
+    conditions: teamName ? [{ field: 'team', operator: '==', value: teamName }] : [],
     orderByField: 'startDate',
     key: refreshKey
   });
-
-  const { data: teams } = useFirebase<Team>('teams');
-
   const fetchAvailableProjects = async () => {
     try {
       const q = query(collection(db, 'projects'));
@@ -66,7 +67,7 @@ export function CompletedProjects({ teamID }: CompletedProjectsProps) {
 
   const handleAddProjectToTeam = async (project: Project) => {
     const team = teams?.find(t => t.id === teamID);
-    const teamName = team?.name ?? 'Équipe inconnue';
+
 
     try {
       await updateDoc(doc(db, 'projects', project.id), {
@@ -128,7 +129,7 @@ export function CompletedProjects({ teamID }: CompletedProjectsProps) {
           <Building2 className="w-5 h-5 mr-2 text-orange-500" />
           Chantiers de l’équipe
         </h3>
-        <motion.button
+        {/* <motion.button
           whileHover={{ scale: 1.02 }}
           whileTap={{ scale: 0.98 }}
           onClick={() => {
@@ -139,7 +140,7 @@ export function CompletedProjects({ teamID }: CompletedProjectsProps) {
         >
           <Building2 className="w-4 h-4 mr-2" />
           Ajouter un chantier
-        </motion.button>
+        </motion.button> */}
       </div>
 
       <div className="flex items-center space-x-2 mb-4">
